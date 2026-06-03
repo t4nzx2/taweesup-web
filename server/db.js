5e08ccb1ef9adede74fa6424db879fc104a041b9
Taweesup <tantaichaiwet@gmail.com>
2026-06-03 08:57:07 +0800

    use persistent disk for database

--- server/db.js
+++ server/db.js
@@ -1,7 +1,12 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const db = new Database(path.join(__dirname, 'hrit.db'));
+// Use DATA_DIR (Render persistent disk) if set, otherwise local folder
+const dbPath = process.env.DATA_DIR
+  ? path.join(process.env.DATA_DIR, 'hrit.db')
+  : path.join(__dirname, 'hrit.db');
+
+const db = new Database(dbPath);
 
 db.pragma('journal_mode = WAL');
 db.pragma('foreign_keys = ON');
