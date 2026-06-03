6ba8fe5d29bd73dd6c10cd7f414d51933665a1ba
Taweesup <tantaichaiwet@gmail.com>
2026-06-04 02:16:33 +0800

    auto-logout on expired token (401)

--- client/src/api.js
+++ client/src/api.js
@@ -12,4 +12,18 @@ api.interceptors.request.use(cfg => {
   return cfg;
 });
 
+// Auto-logout on expired/invalid token (401)
+api.interceptors.response.use(
+  res => res,
+  err => {
+    if (err.response?.status === 401) {
+      localStorage.removeItem('token');
+      if (!window.location.pathname.includes('/login')) {
+        window.location.href = '/login';
+      }
+    }
+    return Promise.reject(err);
+  }
+);
+
 export default api;
